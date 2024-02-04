605db0919cee92749b32b91a2486a2394711d8ca
sanjayr <sanjayr@noc.softcell.com>
2024-02-04 20:03:47 +0530

    routes changes

--- divyanshfilm/film/routes.py
+++ divyanshfilm/film/routes.py
@@ -4,14 +4,14 @@ from . import div_film
 
 @div_film.route('/', methods=['GET','POST'])
 def home():
-    return render_template("divyanshfilm/templates/home.html")
+    return render_template("home.html")
 
 
 @div_film.route('/films', methods=['GET','POST'])
 def films():
-    return render_template("divyanshfilm/templates/films.html", active_menu='films')
+    return render_template("films.html", active_menu='films')
 
 
 @div_film.route('/contact', methods=['GET','POST'])
 def contact():
-    return render_template("divyanshfilm/templates/contact.html", active_menu='contact')
+    return render_template("contact.html", active_menu='contact')

e133b332558738846dfc76eeec3ce7b7999aae1b
sanjayr <sanjayr@noc.softcell.com>
2024-02-05 10:05:32 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from divyanshfilm.film import div_film
 
-app = Flask(__name__, template_folder='divyanshfilm/templates',static_url_path='divyanshfilm/static')
+app = Flask(__name__, template_folder='/divyanshfilm/templates',static_url_path='/divyanshfilm/static')
 
 app.register_blueprint(div_film)
